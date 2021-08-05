1ad95fea802c5224035419c414ade3613963c662
anmol1104 <anmpahwa@ucdavis.edu>
2021-08-05 23:03:50 +0530

    First test set

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
 using TrafficAssignment
 using Test
+using Revise
+using CSV
+using DataFrames
+using BenchmarkTools
 
-@testset "TrafficAssignment.jl" begin
-    # Write your tests here.
+
+testnetworks = ["Anaheim", "SiouxFalls"]
+@testset "FW.jl" begin
+    for network in testnetworks
+        metadata, report, solution = assigntraffic(network, method=:FW, assignment=:UE, tol=1e-10, maxiters=50, maxruntime=300, log=:off)
+        refsol = DataFrame(CSV.File(joinpath(@__DIR__, "Solution\\$network.csv")))
+        rg₁ = 10^report[end, :LOG₁₀RG]
+        rg₂ = abs(TrafficAssignment.getrg(network, refsol))
+        @test rg₁ ≤ rg₂
+    end
 end
