c99cd670d87730477459d8b4eaca0bcb632e1367
anmol1104 <anmpahwa@ucdavis.edu>
2021-09-15 23:16:17 +0530

    First stable release of trafficassignment.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,27 @@
 using TrafficAssignment
 using Test
 using Revise
-using CSV
-using DataFrames
 using BenchmarkTools
+using DataFrames
+using CSV
+using Random
+using Printf
+Random.seed!(1104)
 
-assigntraffic("Anaheim", method=:FW, assignment=:UE, tol=1e-5, maxiters=20, maxruntime=300, log=:on)
-@benchmark assigntraffic("Anaheim", method=:FW, assignment=:UE, tol=1e-5, maxiters=20, maxruntime=300, log=:off)
-
-#TrafficAssignment.compare("Anaheim", methods=[:FW, :fukushimaFW, :conjugateFW])
-
-#=
-testnetworks = ["Anaheim", "SiouxFalls"]
-@testset "FW.jl" begin
+testnetworks = ["Anaheim"]
+algorithms = [:FW, :fukushimaFW, :conjugateFW, :TAPAS]
+@testset "TrafficAssignment" begin
     for network in testnetworks
-        metadata, report, solution = assigntraffic(network, method=:FW, assignment=:UE, tol=1e-10, maxiters=50, maxruntime=300, log=:off)
-        refsol = DataFrame(CSV.File(joinpath(@__DIR__, "Solution\\$network.csv")))
-        rg₁ = 10^report[end, :LOG₁₀RG]
-        rg₂ = abs(TrafficAssignment.getrg(network, refsol))
-        @test rg₁ ≤ rg₂
+        for algorithm in algorithms
+            metadata, report, solution = assigntraffic(network, method=algorithm, assignment=:UE, tol=1e-10, maxiters=50, maxruntime=300, log=:off)
+            refsol = DataFrame(CSV.File(joinpath(@__DIR__, "Solution\\$network.csv")))
+            rg₁ = 10^report[end, :LOG₁₀RG]
+            rg₂ = abs(TrafficAssignment.getrg(network, refsol))
+            println("$network - $algorithm")
+            @printf("   Solution relative gap : %.3e",  rg₁)
+            @printf("\n   Benchmark relative gap: %.3e\n\n",  rg₂)
+            @test rg₁ ≤ rg₂
+        end
     end
 end
-=#
+# TODO: Add more test networks
